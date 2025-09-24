2760606ad98d2ddc4134af3f6a3b3aaee6284a5f
TimSVector <tim.schneider@vector.com>
2025-09-24 12:55:57 -0400

    updates for jenkins

--- IAR/start_vc.bat
+++ IAR/start_vc.bat
@@ -1,9 +1,5 @@
 
-set VCAST_IAR_INSTALL_DIR=D:\vector\tools\iar.9.50\arm
-
-set PATH=%PATH%;%VCAST_IAR_INSTALL_DIR%\bin;%VCAST_IAR_INSTALL_DIR%\..\common\bin
-
-set VCAST_SUPPORT_FILES=%~dp0SupportFiles
+call setenv.bat
 
 
 start /b %VECTORCAST_DIR%\vcastqt -e SnN_IAR_Testing_Campaign
